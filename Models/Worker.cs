b0b4ddd26382e0ad19e110469e9f04c9fdacc246
IrfanShah <71603574+DeveloperIrkhan@users.noreply.github.com>
2021-07-05 12:07:40 +0500

    Add project files.

---
+++ Models/Worker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Models
+{
+    class Worker
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int WorkerId { get; set; }
+
+        [Display(Name = "Worker Name")]
+        [Required(ErrorMessage = "{0} required!!!")]
+        public string WorkerName { get; set; }
+        
+        [Display(Name = "Date of Birth")]
+        [Required(ErrorMessage = "{0} required!!!")]
+        public string Dob { get; set; }
+
+        [Display(Name = "Phone")]
+        [Required(ErrorMessage = "{0} required!!!")] 
+        public string Phone { get; set; }
+        
+        [Display(Name = "Upload Selfie")] 
+        public string Picture { get; set; }
+    }
+}
