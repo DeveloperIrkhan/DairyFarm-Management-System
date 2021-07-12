5009ce6029f06d4d9366c416a6bc24d29fa70013
IrfanShah <71603574+DeveloperIrkhan@users.noreply.github.com>
2021-07-12 22:06:01 +0500

    Some more tables are added

---
+++ Models/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Invoice
+    {
+        public int Invoice_Id  { get; set; }
+        public DateTime Invoice_date { get; set; } = DateTime.Now;
+        public int CostumerId { get; set; }
+        public string CostumerName { get; set; }
+        public string Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public int Total { get; set; }
+    }
+}
